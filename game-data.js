388bcc34a3ab28ed471980513c7a848a026da29d
Austen Valentine <valentine.austen@gmail.com>
2019-05-15 22:49:35 -0400

    first 12 countries and codes in game-data array

---
+++ game-data.js
@@ -0,0 +1,14 @@
+countryCodes = [
+    ["Algeria", "DZ"],
+    ["Angola", "AO"],
+    ["Benin", "BJ"],
+    ["Botswana", "BW"],
+    ["Burkina Faso", "BF"],
+    ["Burundi", "BI"],
+    ["Cameroon", "CM"],
+    ["Cape Verde Islands", "CV"],
+    ["Central African Republic", "CF"],
+    ["Chad", "TD"],
+    ["Comoros", "KM"],
+    ["Congo", "CG"]
+];
