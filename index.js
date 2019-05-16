f43f241e86059fe5b7bd6b2685e79a7a507ad72b
Austen Valentine <valentine.austen@gmail.com>
2019-05-15 23:19:07 -0400

    countries subset ready for card constructor. Card constructor needs component markup.

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 $(function() {
     const jamApp = {};
-    const countryList = [
-        
-    ]
-    const memoCard = function (countryName, countrycode) {
-        this.element = $('div.memo-card');
+
+    const card = function (countryName, countrycode) {
         this.name = countryName;
         this.code = countryCode;
+        this.box = $('div.memo-card');
+        // how is this going to be built?
+        // this.component = this.box.append()
     }
 
-      jamApp.init = function () {
+    const countriesSubset = [];
+    for (let i = 0; i <= 6; i++) {
+        let randIndex = Math.floor(Math.random() * countryCodes.length);
+        let randCountry = countryCodes.splice(randIndex, 1)[0];
+        countriesSubset.push();
+    };
+
+    
+
+    jamApp.init = function () {
         // cache selectors - start screen
         this.startButton = $('button.start');
         this.splash = $('.splash');
