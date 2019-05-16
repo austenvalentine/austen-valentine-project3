6952a4046318354fad0cd8a12023f98a9d9825fe
Austen Valentine <valentine.austen@gmail.com>
2019-05-15 22:05:17 -0400

    all flag svg in assets

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 $(function() {
-    console.log('script is running');
+    const jamApp = {};
+    const countryList = [
+        
+    ]
+    const memoCard = function (countryName, countrycode) {
+        this.element = $('div.memo-card');
+        this.name = countryName;
+        this.code = countryCode;
+    }
+
+      jamApp.init = function () {
+        // cache selectors - start screen
+        this.startButton = $('button.start');
+        this.splash = $('.splash');
+        this.guessDisplay = $('.guess-display');
+
+        this.startButton.on('click', () => {
+            this.splash.removeClass('show');
+            this.splash.addClass('hide');
+        })
+
+        this.gameBoard = $('.game-board');
+        this.setupBoard  = function() {
+
+        }
+    };
+
+    jamApp.init()
+        
+    //     events - splash screen
+    //     Click to start - toggles splash show/hide states
+    
+
 });
