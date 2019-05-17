00a024fb0784b1404d625124e1bd60bac6503c29
Austen Valentine <valentine.austen@gmail.com>
2019-05-17 00:02:41 -0400

    face-down image height fixed; game start method bound to app object

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
 $(function() {
-    const jamApp = {};
-    jamApp.gameBoard = [];
-    // pick the subset of countries to match
-    const countriesSubset = [];
-    for (let i = 0; i < 6; i++) {
-        let randIndex = Math.floor(Math.random() * countryCodes.length);
-        let randCountry = countryCodes.splice(randIndex, 1)[0];
-        countriesSubset.push(randCountry);
-    };
-
-    // scoop up all the cards from the DOM into
-    // a deck of cards - convert the JQuery selector container 
-    // to an array to use built-in array methods
-    const deck = $('.card').toArray();
-    // assign countries to pairs of cards in the deck and deal
-    // them to the virtual board
-    
-    countriesSubset.forEach((country, index) => {
-        // pick a random pair of cards from the deck
-        const cardIndex1 = Math.floor(Math.random() * deck.length);
-        const card1 = deck.splice(cardIndex1, 1);
-        const cardIndex2 = Math.floor(Math.random() * deck.length);
-        const card2 = deck.splice(cardIndex2, 1);
-        // assign current country's flag and name to the pair
-        $(card1).find(".flag").attr(`src`, "assets/flags/" + country[1].toLowerCase() + ".svg");
-        $(card1).find(".name").text(country[0]);
-        $(card2).find(".flag").attr(`src`, "assets/flags/" + country[1].toLowerCase() + ".svg");
-        $(card2).find(".name").text(country[0]);
-
-        jamApp.gameBoard.push(card1, card2);
-    
-    });
-    
-    
-
-    jamApp.init = function () {
-        // cache 
-        this.gameScreen = $('.game-screen');
-        this.startButton = $('button.start');
-        this.splash = $('.splash');
-        this.guessDisplay = $('.guess-display');
-        // dealt cards go in here
-        
-
-        this.startButton.on('click', () => {
+    const jamApp = {
+        // get country data
+        countries: countryCodes,
+        // cache selectors
+        gameScreen: $('.game-screen'),
+        startButton: $('button.start'),
+        splash: $('.splash'),
+        guessDisplay: $('.guess-display'),
+        gameBoard: [],
+        countriesSubset: [],
+        splashGo: function (){
+            console.log(this);
             this.splash.removeClass('show');
             this.splash.addClass('hide');
             this.gameScreen.removeClass('blurred');
-        })
+            this.init(this.countries);
+        },
+        init: function(countries) {
+            // consider using a class instead of init next time
 
+            // countries parameter needs an array of arrays
+            // [[name, country-code],...]
+
+            // pick the subset of countries to match
+            for (let i = 0; i < 6; i++) {
+                const randIndex = Math.floor(Math.random() * countries.length);
+                const randCountry = countries.splice(randIndex, 1)[0];
+                this.countriesSubset.push(randCountry);
+            };
+            // scoop up all the cards from the DOM into
+            // a deck of cards - convert the JQuery selector container 
+            // to an array to use built-in array methods
+            const deck = $('.card').toArray();
+            // assign countries to pairs of cards in the deck and deal
+            // them to the virtual board
+
+            this.countriesSubset.forEach((country, index) => {
+                // pick a random pair of cards from the deck
+                const cardIndex1 = Math.floor(Math.random() * deck.length);
+                const card1 = deck.splice(cardIndex1, 1);
+                const cardIndex2 = Math.floor(Math.random() * deck.length);
+                const card2 = deck.splice(cardIndex2, 1);
+                // assign current country's flag and name to the pair
+                $(card1).find(".flag").attr(`src`, "assets/flags/" + country[1].toLowerCase() + ".svg");
+                $(card1).find(".name").text(country[0]);
+                $(card2).find(".flag").attr(`src`, "assets/flags/" + country[1].toLowerCase() + ".svg");
+                $(card2).find(".name").text(country[0]);
+                this.gameBoard.push(card1, card2);
+            });
+        },
     };
 
-    jamApp.init()
-        
-    //     events - splash screen
-    //     Click to start - toggles splash show/hide states
+
+    // events - splash screen
+    // bind for method to refer to jamApp instead of startButton
+    jamApp.startButton.on('click', jamApp.splashGo.bind(jamApp));
+
+    // launch game
+    jamApp.init(countryCodes);
     
 
 });
