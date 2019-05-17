bc88e28dff9848c6ac0a4f0ce470e9b6f16c44a2
Austen Valentine <valentine.austen@gmail.com>
2019-05-16 21:49:11 -0400

    random card and country selection working - flags and names updating in DOM

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
 $(function() {
     const jamApp = {};
-
-    const card = function (countryName, countrycode) {
-        this.name = countryName;
-        this.code = countryCode;
-        this.component = $('<div>').attr('class', '.memo-card');
-        // nest country info into the component div
-    }
-
+    jamApp.gameBoard = [];
+    // pick the subset of countries to match
     const countriesSubset = [];
-    for (let i = 0; i <= 6; i++) {
+    for (let i = 0; i < 6; i++) {
         let randIndex = Math.floor(Math.random() * countryCodes.length);
         let randCountry = countryCodes.splice(randIndex, 1)[0];
-        countriesSubset.push();
+        countriesSubset.push(randCountry);
     };
 
+    // scoop up all the cards from the DOM into
+    // a deck of cards - convert the JQuery selector container 
+    // to an array to use built-in array methods
+    const deck = $('.card').toArray();
+    // assign countries to pairs of cards in the deck and deal
+    // them to the virtual board
+    
+    countriesSubset.forEach((country, index) => {
+        // pick a random pair of cards from the deck
+        const cardIndex1 = Math.floor(Math.random() * deck.length);
+        const card1 = deck.splice(cardIndex1, 1);
+        const cardIndex2 = Math.floor(Math.random() * deck.length);
+        const card2 = deck.splice(cardIndex2, 1);
+        // assign current country's flag and name to the pair
+        $(card1).find(".flag").attr(`src`, "assets/flags/" + country[1].toLowerCase() + ".svg");
+        $(card1).find(".name").text(country[0]);
+        $(card2).find(".flag").attr(`src`, "assets/flags/" + country[1].toLowerCase() + ".svg");
+        $(card2).find(".name").text(country[0]);
+
+        jamApp.gameBoard.push(card1, card2);
+    
+    });
+    
     
 
     jamApp.init = function () {
-        // cache selectors - start screen
+        // cache 
         this.gameScreen = $('.game-screen');
         this.startButton = $('button.start');
         this.splash = $('.splash');
         this.guessDisplay = $('.guess-display');
+        // dealt cards go in here
+        
 
         this.startButton.on('click', () => {
             this.splash.removeClass('show');
             this.splash.addClass('hide');
             this.gameScreen.removeClass('blurred');
-
         })
 
-
-        this.gameBoard = $('.game-board');
-        this.setupBoard  = function() {
-
-        }
     };
 
     jamApp.init()
